43aa725757135c31dbe235a5164a4da1db321b1f
_justParrot <contact@justparrot.me>
2026-09-09 21:21:51 +0200

    fix

--- src/app.js
+++ src/app.js
@@ -17,4 +17,4 @@ app.use('/lock/admin', tokenRoute)
 app.use('/lock', activateRoute)
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Auth API running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Auth API running on port ${PORT}`));
